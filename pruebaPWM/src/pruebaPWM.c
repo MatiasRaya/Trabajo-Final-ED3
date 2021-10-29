21dcc42f56e84817acf816269d8ddab9913a333f
MatiasRaya <matias.raya@mi.unc.edu.ar>
2021-10-29 19:42:31 -0300

    Se agrega prueba de paso de datos a registros MATCH de PWM

--- pruebaPWM/src/pruebaPWM.c
+++ pruebaPWM/src/pruebaPWM.c
@@ -1,35 +1,68 @@
-/*
-===============================================================================
- Name        : pruebaPWM.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
-===============================================================================
-*/
-
-#ifdef __USE_CMSIS
 #include "LPC17xx.h"
-#endif
 
-#include <cr_section_macros.h>
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_pwm.h"
 
-// TODO: insert other include files here
+void configPin();
+void configPWM();
 
-// TODO: insert other definitions and declarations here
+uint32_t valor0, valor1;
 
-int main(void) {
+int main()
+{
+    configPin();
+	configPWM();
 
-    // TODO: insert code here
+    while(1){}
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
-    }
-    return 0 ;
+    return 0;
+}
+
+void configPin()
+{
+    PINSEL_CFG_Type PinCfg;
+
+	//Configuramos PWM
+	PinCfg.Funcnum = 1;                                                 //Se selecciona la segunda funcion del pin
+	PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;                           //Se selecciona el modo normal del pin, el drenador esta cerrado
+	PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;                             //Se selecciona la resistencia pull-up del pin
+	PinCfg.Pinnum = 0;                                                  //Se selecciona el pin a usar
+	PinCfg.Portnum = 2;                                                 //Se selecciona el puerto
+	PINSEL_ConfigPin(&PinCfg);                                          //Se pasa PinCfg para el puerto P2.0
+	PinCfg.Pinnum = 1;                                                  //Se selecciona el pin a usar
+	PINSEL_ConfigPin(&PinCfg);                                          //Se pasa PinCfg para el puerto P2.1
+
+
+    return;
+}
+
+//Configuracion PWM
+void configPWM()
+{
+	PWM_TIMERCFG_Type TymerPWMConfig;
+	PWM_MATCHCFG_Type MatchPWMConfig;
+
+	TymerPWMConfig.PrescaleOption = PWM_TIMER_PRESCALE_TICKVAL;			//Se coloca el prescaler en valor absoluto
+	TymerPWMConfig.PrescaleValue = 25;									//Se selecciona el valor del prescaler
+	MatchPWMConfig.IntOnMatch = DISABLE;								//Se desactiva la interrupcion al hacer MATCH
+	MatchPWMConfig.MatchChannel = 0;									//Se selecciona el canal de MATCH
+	MatchPWMConfig.ResetOnMatch = ENABLE;								//Se activa el RESET después de hacer MATCH
+	MatchPWMConfig.StopOnMatch = DISABLE;								//Se desactiva la detencion despues de hacer MATCH
+	PWM_ConfigMatch(LPC_PWM1,&MatchPWMConfig);							//Se pasa MatchPWMConfig del MATCH0
+	MatchPWMConfig.ResetOnMatch = DISABLE;								//Se desactiva el RESET después de hacer MATCH
+	MatchPWMConfig.MatchChannel = 1;									//Se selecciona el canal de MATCH
+	PWM_ConfigMatch(LPC_PWM1,&MatchPWMConfig);							//Se pasa MatchPWMConfig del MATCH1
+
+	PWM_Init(LPC_PWM1,PWM_MODE_TIMER,&TymerPWMConfig);					//Se inicializa el periferico
+	PWM_MatchUpdate(LPC_PWM1,0,20000,PWM_MATCH_UPDATE_NOW);				//Se configura el MATCH0 en 20ms
+	PWM_MatchUpdate(LPC_PWM1,1,20,PWM_MATCH_UPDATE_NOW);
+	//El canal 1 no es necesario configurarlo ya que por solo trabaja en el modo de flanco unico
+	PWM_ChannelCmd(LPC_PWM1,1,ENABLE);									//Se habilita el canal 1
+	PWM_CounterCmd(LPC_PWM1,ENABLE);									//Se habilita el contador del periferico
+	PWM_Cmd(LPC_PWM1, ENABLE);											//Se habilita el periferico
+
+
+	valor0 = LPC_PWM1->MR0;
+	valor1 = LPC_PWM1->MR1;
+	return;
 }
